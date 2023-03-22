da6d169422872096c4b9808e057430538fc6e5c5
Ethan Wulf <32464090+ethanwulf@users.noreply.github.com>
2023-03-22 15:15:52 -0400

    Create name_gen.py

---
+++ name_gen.py
@@ -0,0 +1,4 @@
+print("Welcome to the name generator!")
+fav_color = input("What is your favorite color?\n")
+fav_animal = input("What is your favorite animal?\n")
+print("Your cool new name is " + fav_color + " " + fav_animal + "!")
